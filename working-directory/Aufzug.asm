cc6602c756c8f29401e8512ddea43acd32a4eb2d
Christian Verdion <der_Chris@gmx.org>
2015-01-29 10:19:27 +0100

    Fixed Some Errors... more to come

--- working-directory/Aufzug.asm
+++ working-directory/Aufzug.asm
@@ -36,22 +36,19 @@
 ; R1	Button 1 has been pressed
 ; R2	Button 2 has been pressed
 ; R7	Direction the Elevator is headding. R7 = 0 -> up, R7 = 1 -> down
-
-
-CSEG AT 0100H
+CSEG AT 00H
 LJMP init_interrupts
-
 
 ; Interrupt Address for ExternalInterrupt0 -> Emergency Button
 ORG 03h
-LCALL ext0 ; try LCALL. If error use CALL instead
+LCALL ext0
 RETI
 
 
 ; Interrupt Address for Timer0
 ; This Interrupt checks every 5ms if the Sensors are triggered
 ORG 0bh
-LCALL timer0 ; try LCALL. If error use CALL instead
+LCALL timer0
 RETI
 
 
@@ -61,7 +58,7 @@ ORG 01bh
 LCALL timer1 ; try LCALL. If error use CALL instead
 RETI
 
-
+CSEG AT 0100H
 ;initialize interrupts
 init_interrupts:
 MOV TMOD, 00010001b ;Timer1: Gate1=0, C/T1 = 0, Mode1 = 01; Timer0: Gate0=0, C/T0 = 0, Mode0 = 01;
@@ -84,21 +81,26 @@ init:
 MOV P1, 00h ; turn off Engines -> Engines are low active
 MOV P3, 0ffh ; MOV P3, #0ffh ;turn off Display -> Display is low active
 
-CLR R0 ; R0 = 1, if Button for First Floor has been pressed
-CLR R1 ; R1 = 1, if Button for Second Floor has been pressed
-CLR R2 ; R2 = 1, if Button for Third Floor has been pressed
+MOV R0, 00h ; R0 = 1, if Button for First Floor has been pressed
+MOV R1, 00h ; R1 = 1, if Button for Second Floor has been pressed
+MOV R2, 00h ; R2 = 1, if Button for Third Floor has been pressed
 
-CALL initialCheckFloor
-CALL checkFloor
+LCALL initialCheckFloor
+JMP checkFloor
 
-END
+
+
+; do something usefull
+ext0:
+MOV P3, 10000110b ;Activate LED´s for Error
+RET
 
 
 ; Check sensors, if sensors are triggered Or OpenDoor or SameFloor Buttons are pressed,open the doors
 timer0:
 MOV A, P3
 ANL A, 00001100b ; If P3.2 or P3.3 is triggered, then Accu != 0
-AZ checkDoorButtons ; And if Accu = 0, countinue
+JZ checkDoorButtons ; And if Accu = 0, countinue
 CLR P1.0
 SETB P1.1 ; stop doors from closing, open doors
 MOV A, P2
@@ -138,7 +140,7 @@ ANL A, 00000001b
 JZ closeDoorsThirdFloor ; when doors are not closed, wait till they are
 JMP initTimer0
 ; in the end, Timer 0 has to be reinitialized
-initTimer0
+initTimer0:
 MOV TL0, 0F9h ; 249 in Low von Timer0
 MOV TH0, 09h ; 9 in High von Timer0
 RET
@@ -148,28 +150,22 @@ RET
 timer1:
 MOV A, P0
 ANL A, 00001001b ; If P0.0 or P0.3 is pressed, then Accu != 0
-AZ checkSecondFloorButton ; And if Accu = 0, then check other floor
+JZ checkSecondFloorButton ; And if Accu = 0, then check other floor
 MOV R0, 01h
 checkSecondFloorButton:
 MOV A, P0
 ANL A, 00010010b ; If P0.1 or P0.4 is pressed, then Accu != 0
-AZ checkThirdFloorButton ; And if Accu = 0, then check other floor
+JZ checkThirdFloorButton ; And if Accu = 0, then check other floor
 MOV R1, 01h
 checkThirdFloorButton:
 MOV A, P0
 ANL A, 00100100b ; If P0.2 or P0.5 is pressed, then Accu != 0
-AZ initTimer1: ; And if Accu = 0, then check other floor
+JZ initTimer1 ; And if Accu = 0, then check other floor
 MOV R2, 01h
 initTimer1:
 ; in the end, Timer 1 has to be reinitialized
 MOV TL1, 0F9h ; 249 in Low von Timer1
 MOV TH1, 013h ; 19 in High von Timer1
-RET
-
-
-ext1:
-; do something usefull
-MOV P3, 10000110b ;Activate LED´s for Error
 RET
 
 
@@ -225,9 +221,9 @@ CALL openDoors
 ; here could be a timer
 CALL closeDoors
 ; Check if we are on top or at the bottom. If we are at the button, first try getting up.
-CJNE R0, 00h, goingUp
-CJNE R2, 00h, goingDown
-CJNE R7, 00h, goingDown
+CJNE R0, #00h, goingUp
+CJNE R2, #00h, goingDown
+CJNE R7, #00h, goingDown
 JMP goingUp
 
 
@@ -279,7 +275,7 @@ ORL A, 00000010b
 MOV IE, A ; Activate interrupt to Check DoorSensors 
 CLR P1.0
 SETB P1.1
-JNB P2.1 closeDoores ; If Doores Are Closed STOP InterruptTimer and Return
+JNB P2.1, closeDoores ; If Doores Are Closed STOP InterruptTimer and Return
 dooresClosed:
 MOV A, IE
 ANL A, 11111101b
@@ -305,6 +301,21 @@ MOV A, R2
 JNZ goToThirdFloor
 ; if no button is pressed, check if button is pressed to drive down
 JMP goingDown
+
+
+showFirstFloor:
+MOV P3, 11111001b ;Activate LED´s for first Floor
+RET
+
+
+showSecondFloor:
+MOV P3, 10100100b ;Activate LED´s for second Floor
+RET
+
+
+showThirdFloor:
+MOV P3, 10110000b ;Activate LED´s for third Floor
+RET
 
 
 END
